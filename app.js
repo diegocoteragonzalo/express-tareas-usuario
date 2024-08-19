2623746e4957781e30896e897f7819d40ed819c6
Diego <diegocotera@gmail.com>
2024-08-19 19:42:12 +0200

    /, /tasks and /users

---
+++ app.js
@@ -0,0 +1,34 @@
+let express = require("express")
+let app = express()
+
+
+app.get("/", (req, res) => res.send("¡Funciona!"))
+
+
+let tasks = [
+	{ id:1, text:"tarea 1"},
+	{ id:2, text:"tarea 2"},
+	{ id:3, text:"tarea 3"}
+]
+
+
+let users = [
+	{ id:1, email:"user1@correo.com", password:"pass1" },
+	{ id:2, email:"user2@correo.com", password:"pass2" },
+	{ id:3, email:"user3@correo.com", password:"pass3" }
+]
+
+
+app.get("/tasks", (req, res) => {
+	res.send(tasks)
+})
+
+
+app.get("/users", (req, res) => {
+	res.send(users)
+})
+
+
+app.listen(8081, () => {
+	console.log("Server running in port 8081")
+})
